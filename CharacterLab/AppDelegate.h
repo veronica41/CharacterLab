9a5916f456b253006c1f17a84ddafd7601f56d5c
pbacci <pierpaolo@pierpaolo-mbp.corp.dropbox.com>
2014-07-03 18:56:14 -0700

    Start project

---
+++ CharacterLab/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CharacterLab
+//
+//  Created by Pierpaolo Baccichet on 7/3/14.
+//  Copyright (c) 2014 Dropbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
