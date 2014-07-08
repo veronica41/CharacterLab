6befd19fff842f2ed132b9a93aefbd16eeeb39f1
Veronica Zheng <zhengzhixing@gmail.com>
2014-07-08 02:03:42 -0700

    add TraitDetailViewController

---
+++ CharacterLab/TraitDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  TraitDetailViewController.h
+//  CharacterLab
+//
+//  Created by Veronica Zheng on 7/7/14.
+//  Copyright (c) 2014 Dropbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TraitDetailViewController : UIViewController
+
+@end
---
+++ CharacterLab/TraitDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  TraitDetailViewController.m
+//  CharacterLab
+//
+//  Created by Veronica Zheng on 7/7/14.
+//  Copyright (c) 2014 Dropbox. All rights reserved.
+//
+
+#import "TraitDetailViewController.h"
+
+@interface TraitDetailViewController ()
+
+@end
+
+@implementation TraitDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
