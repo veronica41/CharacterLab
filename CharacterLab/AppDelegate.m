3858340c54d14ae5d3f17fb06beef16748b3c8d5
Rajeev Nayak <jeev.nayak@gmail.com>
2014-07-15 00:46:50 -0700

    black background for flip transition

--- CharacterLab/AppDelegate.m
+++ CharacterLab/AppDelegate.m
@@ -41,7 +41,7 @@
         self.window.rootViewController = self.mainViewController;
     }
 
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor blackColor];
     [self.window makeKeyAndVisible];
     return YES;
 }
